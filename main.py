82c869b36ce65e43c81783e4fbfc59203b484012
hallelhel <hallelh@post.bgu.ac.il>
2021-10-12 12:27:38 +0300

    visualization changes

--- main.py
+++ main.py
@@ -3,68 +3,97 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
+import io
+from base64 import encodebytes
+from PIL import Image
+
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+
 
 @app.route('/update_cluster',methods=['POST'])
 def update_cluster():
   n_cluster = request.form['number']
   print(n_cluster)
   if n_cluster:
-    k = n_cluster
-
-    def retrieve_info(cluster_labels, y_train):
-      # Initializing
-      reference_labels = {}
-      # For loop to run through each label of cluster label
-      for i in range(len(np.unique(kmeans.labels_))):
-        index = np.where(cluster_labels == i, 1, 0)
-        num = np.bincount(y_train[index == 1]).argmax()
-        reference_labels[i] = num
-      return reference_labels
-
+    k = int(n_cluster)
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    # Normalization of ‘x_train’
-    x_train = x_train.astype('float32')
-    x_train = x_train / 255.0
-    # Reshaping of ‘x_train’
-    x_train = x_train.reshape(60000, 28 * 28)
-    # Training the model
+    x_test_as_mat = x_test
 
+    # Normalization of ‘x_test’
+    x_test = x_test.astype('float32')
+    x_test = x_test / 255.0
+    # Reshaping of ‘x_test’
+    x_test = x_test.reshape(10000, 28 * 28)
+    # Training the model
     kmeans = KMeans(n_clusters=k)
-    kmeans.fit(x_train)
-    reference_labels = retrieve_info(kmeans.labels_, y_train)
-    number_labels = np.random.rand(len(kmeans.labels_))
-    for i in range(len(kmeans.labels_)):
-      number_labels[i] = reference_labels[kmeans.labels_[i]]
+    kmeans.fit(x_test)
+    reference_labels = retrieve_info(kmeans, kmeans.labels_, y_test)
 
     # Cluster centroids is stored in ‘centroids’
     centroids = kmeans.cluster_centers_
+    centroids = centroids.reshape(k, 28, 28) # to see centroid as image
+    centroids = centroids * 255 # nullify the normalization effect
 
-    centroids = centroids.reshape(k, 28, 28)
-    centroids = centroids * k
-    plt.figure(figsize=(10, 9))
-    bottom = 0.35
-
+    # for 5 other images
+    indexes_of_img_to_show = {}
     for i in range(k):
-      plt.subplots_adjust(bottom)
-      plt.subplot(4, 4, i + 1)
-      plt.title('Number:{}'.format(reference_labels[i]), fontsize=17)
-      plt.imshow(centroids[i])
-      plt.show()
-      plt.savefig('{}.png'.format(reference_labels[i]))
+      array_of_index = np.where(kmeans.labels_ == i)
+      indexes_of_img_to_show[i] = array_of_index[0][:5]
 
+    loc = 0
+    fig = plt.figure(figsize=(10, 9))
+    for i in range(k):
+      sub = fig.add_subplot(k, 6, loc+1)
+      sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=10,loc='left',pad=-0.1)
+      sub.imshow(centroids[i])
+      plt.axis('off')
+      loc = loc + 1
 
-    return jsonify({'k': k})
+      for j in indexes_of_img_to_show[i]:
+        sub = fig.add_subplot(k, 6, loc + 1)
+        # sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=8)
+        sub.imshow(x_test_as_mat[j])
+        plt.axis('off')
+        loc = loc + 1
+
+    plt.savefig('cluster.png')
+    image_path = 'cluster.png'
+    encoded_img = get_response_image(image_path)
+    return jsonify({'image': encoded_img})
+
   return jsonify({'error': 'Please enter a digit'})
 
 
+'''
+    Associates most probable label with each cluster in KMeans model
+    returns: dictionary of clusters assigned to each label
+    '''
 
+
+def retrieve_info(kmeans, cluster_labels, y_test):
+  # Initializing
+  reference_labels = {}
+  # For loop to run through each label of cluster label
+  for i in range(len(np.unique(kmeans.labels_))):
+    index = np.where(cluster_labels == i, 1, 0)
+    num = np.bincount(y_test[index == 1]).argmax()
+    reference_labels[i] = num
+  return reference_labels
+
+def get_response_image(image_path):
+  pil_img = Image.open(image_path, mode='r')  # reads the PIL image
+  byte_arr = io.BytesIO()
+  pil_img.save(byte_arr, format='PNG')  # convert the PIL image to byte array
+  encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')  # encode as base64
+  return encoded_img
 
 
 
--- static/form.js
+++ static/form.js
@@ -5,26 +5,30 @@ $(document).ready(function() {
 		$.ajax({
 			data : {
 				number : $('#numberInput').val()
-
 			},
 			type : 'POST',
-			url : '/update_cluster'
+			url : '/update_cluster',
+
 		})
 		.done(function(data) {
 
 			if (data.error) {
 				$('#errorAlert').text(data.error).show();
 				$('#successAlert').hide();
+
 			}
 			else {
-				$('#successAlert').text(data.number).show();
-				$('#errorAlert').hide();
+				$('#img_div').html('<img src="data:image/png;base64,' + data.image + '" />');
+                $('#errorAlert').hide();
 			}
 
 		});
 
 		event.preventDefault();
 
+
 	});
+
+
 
 });
