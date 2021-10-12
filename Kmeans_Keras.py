1296ce1b79814574b35e10e179a358117b60aa22
hallelhel <hallelh@post.bgu.ac.il>
2021-10-12 12:59:00 +0300

    visualization changes for label

--- Kmeans_Keras.py
+++ Kmeans_Keras.py
@@ -10,175 +10,76 @@ from skimage import io
 import math
 
 
-def retrieve_info(cluster_labels, y_train):
+def retrieve_info(cluster_labels, y_test):
     # Initializing
     reference_labels = {}
     # For loop to run through each label of cluster label
     for i in range(len(np.unique(kmeans.labels_))):
         index = np.where(cluster_labels == i, 1, 0)
-        num = np.bincount(y_train[index == 1]).argmax()
+        num = np.bincount(y_test[index == 1]).argmax()
         reference_labels[i] = num
     return reference_labels
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 # Normalization of ‘x_train’
-x_train = x_train.astype('float32')
-x_train = x_train / 255.0
+x_test_as_mat = x_test
+x_test = x_test.astype('float32')
+x_test = x_test / 255.0
 # Reshaping of ‘x_train’
-x_train = x_train.reshape(60000, 28 * 28)
+x_test = x_test.reshape(10000, 28 * 28)
 # Training the model
-k = 10
+k = 4
 kmeans = KMeans(n_clusters=k)
-kmeans.fit(x_train)
-reference_labels = retrieve_info(kmeans.labels_, y_train)
-number_labels = np.random.rand(len(kmeans.labels_))
-for i in range(len(kmeans.labels_)):
-    number_labels[i] = reference_labels[kmeans.labels_[i]]
+kmeans.fit(x_test)
+reference_labels = retrieve_info(kmeans.labels_, y_test)
+# ‘number_labels’ is a list which denotes the number displayed in image
+# number_labels = np.random.rand(len(kmeans.labels_))
+# for i in range(len(kmeans.labels_)):
+#     number_labels[i] = reference_labels[kmeans.labels_[i]]
 
 # Cluster centroids is stored in ‘centroids’
 centroids = kmeans.cluster_centers_
 
 centroids = centroids.reshape(k, 28, 28)
-centroids = centroids * k
+centroids = centroids * 255
 fig = plt.figure(figsize=(10, 9))
-bottom = 0.35
-row = math.floor(math.sqrt(k))
-col = math.ceil(math.sqrt(k))
+
+# bottom = 0.35
+# row = math.floor(math.sqrt(k))
+# col = math.ceil(math.sqrt(k))
 for i in range(k):
     # plt.subplots_adjust(bottom)
     sub = fig.add_subplot(k,4,i+1)
     # plt.subplot(4, 4, i + 1)
     sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=12)
     sub.imshow(centroids[i])
+    plt.axis('off')
     # plt.savefig('{}.png'.format(reference_labels[i]))
     # plt.show()
+plt.savefig('cluster.png')
 plt.show()
 
 
+# def five_more_img():
+indexes = {}
+for i in range(k):
+    array_of_index = np.where(kmeans.labels_ == i)
+    indexes[i] = array_of_index[0][:5]
+    # return indexes
 
+indexes_of_img_to_show = indexes
 
+fig_5 = plt.figure(figsize=(10, 9))
 
+loc = 0
+for i in range(k):
+    for j in indexes_of_img_to_show[i]:
+        sub_5 = fig_5.add_subplot(k, 5, loc + 1)
+        sub_5.set_title('Number:{}'.format(reference_labels[i]), fontsize=12)
+        sub_5.imshow(x_test_as_mat[j])
+        plt.axis('off')
+        loc=loc+1
+plt.savefig('5_img.png')
+plt.show()
 
-
-#
-#
-#
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# #
-# # print(type(x_train))
-# # print(type(x_test))
-# # print(type(y_train))
-# # print(type(y_test))
-# #
-# # print(x_train.shape)
-# # print(x_test.shape)
-# # print(y_train.shape)
-# # print(y_test.shape)
-# #
-# plt.gray() # B/W Images
-# plt.figure(figsize = (10,9)) # Adjusting figure size
-# # Displaying a grid of 3x3 images
-# for i in range(9):
-#  plt.subplot(3,3,i+1)
-#  plt.imshow(x_train[i])
-#  plt.show()
-#
-# for i in range(5):
-#   print(y_train[i])
-#
-#
-# # Data Normalization
-# # Conversion to float
-# x_train = x_train.astype('float32')
-# x_test = x_test.astype('float32')
-# # Normalization
-# x_train = (x_train)/255.0
-# x_test = (x_test)/255.0
-#
-# X_train = x_train.reshape(len(x_train),-1)
-# X_test = x_test.reshape(len(x_test),-1)
-#
-# total_clusters = len(np.unique(y_test))
-# # total_clusters = 9
-# # Initialize the K-Means model
-# kmeans = KMeans(n_clusters = total_clusters)
-# # Fitting the model to training set
-# kmeans.fit(X_train)
-#
-# # print(kmeans.labels_)
-#
-# '''
-#  Associates most probable label with each cluster in KMeans model
-#  returns: dictionary of clusters assigned to each label
-# '''
-# def retrieve_info(cluster_labels,y_train):
-#     # Initializing
-#     reference_labels = {}
-#     # For loop to run through each label of cluster label
-#     for i in range(len(np.unique(kmeans.labels_))):
-#         index = np.where(cluster_labels == i,1,0)
-#         num = np.bincount(y_train[index==1]).argmax()
-#         reference_labels[i] = num
-#     return reference_labels
-#
-# reference_labels = retrieve_info(kmeans.labels_,y_train)
-# number_labels = np.random.rand(len(kmeans.labels_))
-# for i in range(len(kmeans.labels_)):
-#   number_labels[i] = reference_labels[kmeans.labels_[i]]
-# print(number_labels[:20].astype('int'))
-# print(y_train[:20])
-#
-# # Testing model on Testing set
-# # Initialize the K-Means model
-# kmeans = KMeans(n_clusters = 256)
-# # Fitting the model to testing set
-# kmeans.fit(X_test)
-# reference_labels = retrieve_info(kmeans.labels_,y_test)
-# number_labels = np.random.rand(len(kmeans.labels_))
-# for i in range(len(kmeans.labels_)):
-#     number_labels[i] = reference_labels[kmeans.labels_[i]]
-#
-# # Cluster centroids is stored in ‘centroids’
-# centroids = kmeans.cluster_centers_
-# centroids = centroids.reshape(256,28,28)
-# centroids = centroids * 255
-# plt.figure(figsize = (10,9))
-# bottom = 0.35
-# for i in range(16):
-#  plt.subplots_adjust(bottom)
-#  plt.subplot(4,4,i+1)
-#  plt.title('Number:{}'.format(reference_labels[i]),fontsize = 17)
-#  plt.imshow(centroids[i])
-#
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-#
-# # Normalization of ‘x_train’
-# x_train = x_train.astype('float32')
-# x_train = x_train/255.0
-# # Reshaping of ‘x_train’
-# x_train = x_train.reshape(60000,28*28)
-# # Training the model
-# n_clusters = 9
-# kmeans = KMeans(n_clusters= n_clusters)
-# kmeans.fit(x_train)
-# reference_labels = retrieve_info(kmeans.labels_,y_train)
-# number_labels = np.random.rand(len(kmeans.labels_))
-# for i in range(len(kmeans.labels_)):
-#   number_labels[i] = reference_labels[kmeans.labels_[i]]
-# # predicted_cluster = kmeans.predict(image)
-# # number_labels[[predicted_cluster]]
-#
-# # Cluster centroids is stored in ‘centroids’
-# centroids = kmeans.cluster_centers_
-#
-# centroids = centroids.reshape(n_clusters,28,28)
-# centroids = centroids * n_clusters
-# plt.figure(figsize = (10,9))
-# bottom = 0.35
-# for i in range(n_clusters):
-#  plt.subplots_adjust(bottom)
-#  plt.subplot(4,4,i+1)
-#  plt.title('Number:{}'.format(reference_labels[i]),fontsize = 17)
-#  plt.imshow(centroids[i])
-#  plt.show()
--- main.py
+++ main.py
@@ -50,20 +50,21 @@ def update_cluster():
 
     loc = 0
     fig = plt.figure(figsize=(10, 9))
+
     for i in range(k):
       sub = fig.add_subplot(k, 6, loc+1)
-      sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=10,loc='left',pad=-0.1)
+      sub.set_title('Number:{} center'.format(reference_labels[i]), fontsize=10,loc='left',pad=-0.1)
       sub.imshow(centroids[i])
       plt.axis('off')
       loc = loc + 1
 
       for j in indexes_of_img_to_show[i]:
         sub = fig.add_subplot(k, 6, loc + 1)
-        # sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=8)
+        sub.set_title('Number:{}'.format(reference_labels[i]), fontsize=8)
         sub.imshow(x_test_as_mat[j])
         plt.axis('off')
         loc = loc + 1
-
+    fig.tight_layout()
     plt.savefig('cluster.png')
     image_path = 'cluster.png'
     encoded_img = get_response_image(image_path)
